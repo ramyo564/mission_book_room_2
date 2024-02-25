3d0a21e6970acab286a5126e4d658c5dbf2c322e
ramyo564 <yohan032yohan@gmail.com>
2024-02-26 00:44:17 +0900

    User 레포지토리, 엔티티 구성

---
+++ src/main/java/com/book/persist/UserRepository.java
@@ -0,0 +1,14 @@
+package com.book.persist;
+
+import com.book.persist.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository
+        extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByPhoneNumber(String phoneNumber);
+
+    boolean existsByPhoneNumber(String phoneNumber);
+}
---
+++ src/main/java/com/book/persist/entity/UserEntity.java
@@ -0,0 +1,67 @@
+package com.book.persist.entity;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name= "users")
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String phoneNumber;
+    private String password;
+    private Timestamp createdAt;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "member_roles", joinColumns = @JoinColumn(name = "member_id"))
+    @Column(name = "role")
+    private List<String> roles;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return phoneNumber;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
