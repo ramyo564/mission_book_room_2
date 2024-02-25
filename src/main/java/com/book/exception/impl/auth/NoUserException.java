fcb76d63748ba9802cf7d2dc58f744f0d0373c6b
ramyo564 <yohan032yohan@gmail.com>
2024-02-26 00:45:29 +0900

    예외처리

---
+++ src/main/java/com/book/exception/impl/auth/NoUserException.java
@@ -0,0 +1,16 @@
+package com.book.exception.impl.auth;
+
+import com.book.exception.AbstractException;
+import org.springframework.http.HttpStatus;
+
+public class NoUserException extends AbstractException {
+    @Override
+    public int getStatusCode() {
+        return HttpStatus.BAD_REQUEST.value();
+    }
+
+    @Override
+    public String getMessage() {
+        return "존재하지 않는 ID입니다.";
+    }
+}
